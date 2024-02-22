f2adf42e4d034564d9a511bbcdf570fbfe486cbb
Melanie Magno <MAGNOMK@ETSU.EDU>
2024-02-21 23:05:19 -0500

    updated menu options to be better optimalized

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -4,32 +4,37 @@
 /// pulling a get request from an API
 /// 2/21/2024
 ///
-
-
-
-
 using lab5;
+
 AmiiboAPI api = new AmiiboAPI();
+bool getOut = false;
 
-Console.WriteLine("do you want to: \n1. print a list of the amiibos \n2. print a single amiibo based on its' name?");
-string answer = Console.ReadLine();
-switch(answer)
+while (!getOut)
 {
-    //if user wants to print list of amiibos
-    case "1":
-        HttpResponseMessage response = await api.GetAmiiboNames();
+    Console.WriteLine("do you want to: \n\t1. print a list of the amiibos \n\t2. print a single amiibo based on its' name?\n\t3. Exit");
+    string answer = Console.ReadLine();
+    switch (answer)
+    {
+        //if user wants to print list of amiibos
+        case "1":
+            HttpResponseMessage response = await api.GetAmiiboNames();
 
-        //should use if(response?.IsSuccessStatusCode) in case the request isnt successful
-        var list = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(list);
-        break;
-    case "2":
-        Console.WriteLine("what amiibo do you want to see?");
-        string value = Console.ReadLine().ToLower();
-        HttpResponseMessage responseWithName = await api.GetAmiiboInfoBasedOnName("name", value);
-        var amiibo = await responseWithName.Content.ReadAsStringAsync();
-        Console.WriteLine(amiibo);
-        break;
-    default:
-        break;
-}    
+            //should use if(response?.IsSuccessStatusCode) in case the request isnt successful
+            var list = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(list);
+            break;
+        case "2":
+            Console.WriteLine("what amiibo do you want to see?");
+            string value = Console.ReadLine().ToLower();
+            HttpResponseMessage responseWithName = await api.GetAmiiboInfoBasedOnName("name", value);
+            var amiibo = await responseWithName.Content.ReadAsStringAsync();
+            Console.WriteLine(amiibo);
+            break;
+        case "3":
+            getOut=true;
+            break;
+        default:
+            Console.WriteLine("try again");
+            break;
+    }
+}
